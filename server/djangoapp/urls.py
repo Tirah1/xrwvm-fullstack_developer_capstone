5af0d30879a2ea388e0827043b23d39924e532cb
JUSTINA NGINA NDUNDA <nginajustina1@gmail.com>
2024-11-21 08:45:56 +0300

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,22 +5,40 @@ from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-
-    # # path for registration
+    # Path for registration
     path(route='register', view=views.registration, name='register'),
-    # path for login
+    # Path for login
     path(route='login', view=views.login_user, name='login'),
+    # Path for logout
     path(route='logout', view=views.logout_request, name='logout'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path('get_dealers/<str:state>/', views.get_dealerships, name='get_dealers'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-
- 
-    # path for dealer reviews view
+    # Path for fetching cars
+    path(route='get_cars', view=views.get_cars, name='get_cars'),
+    # Path for getting dealerships
+    path(
+        route='get_dealers/',
+        view=views.get_dealerships,
+        name='get_dealers'
+    ),
+    # Path for getting dealerships by state
+    path(
+        'get_dealers/<str:state>/',
+        views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+    # Path for dealer details
+    path(
+        route='dealer/<int:dealer_id>',
+        view=views.get_dealer_details,
+        name='dealer_details'
+    ),
+    # Path for dealer reviews
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='dealer_reviews'
+    ),
+    # Path for adding a review
     path(route='add_review', view=views.add_review, name='add_review'),
-    # path for add a review view
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
