7dcd9e92eb0b05701c4940df02bf64df738a895b
JUSTINA NGINA NDUNDA <nginajustina1@gmail.com>
2024-11-21 10:49:54 +0300

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -128,18 +128,4 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
-MEDIA_URL = '/media/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/static'),
-    os.path.join(BASE)
+#
