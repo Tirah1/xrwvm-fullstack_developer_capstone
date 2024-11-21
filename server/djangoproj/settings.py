bccc40987ec319fc22d647e9d0e28a5fdd50d45b
JUSTINA NGINA NDUNDA <nginajustina1@gmail.com>
2024-11-21 11:20:27 +0300

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -22,27 +22,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
-)
+SECRET_KEY = 'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost', 
-    '127.0.0.1', 
-    (
-        'nginajustina-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
-        'proxy.cognitiveclass.ai'
-    ),
+    'localhost',
+    '127.0.0.1',
+    ('nginajustina-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.'
+     'cognitiveclass.ai')
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    (
-        'https://nginajustina-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
-        'proxy.cognitiveclass.ai'
-    ),
+    ('https://nginajustina-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
+     'proxy.cognitiveclass.ai')
 ]
 
 REST_FRAMEWORK = {
@@ -107,28 +101,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'UserAttributeSimilarityValidator'
-        ),
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'MinimumLengthValidator'
-        ),
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'CommonPasswordValidator'
-        ),
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'NumericPasswordValidator'
-        ),
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
