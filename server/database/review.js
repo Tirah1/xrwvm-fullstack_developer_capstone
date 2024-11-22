868179dace1233ad3ddb80aa2079bb31874e5f59
Tirah1 <nginajustina1@gmail.com>
2024-11-22 01:08:07 -0500

    new changes

--- server/database/review.js
+++ server/database/review.js
@@ -2,52 +2,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Dealerships schema
-const dealerships = new Schema({
-  id: {
-    type: Number,
-    required: true,
-  },
-  city: {
-    type: String,
-    required: true
-  },
-  state: {
-    type: String,
-    required: true
-  },
-  address: {
-    type: String,
-    required: true
-  },
-  zip: {
-    type: String,
-    required: true
-  },
-  lat: {
-    type: String,
-    required: true
-  },
-  long: {
-    type: String,
-    required: true
-  },
-  short_name: {
-    type: String,
-  },
-  full_name: {
-    type: String,
-    required: true
-  }
-});
-
-// Reviews schema
 const reviews = new Schema({
-  id: {
+	id: {
     type: Number,
     required: true,
-  },
-  name: {
+	},
+	name: {
     type: String,
     required: true
   },
@@ -81,8 +41,4 @@ const reviews = new Schema({
   },
 });
 
-// Export both models
-module.exports = {
-  Dealership: mongoose.model('dealerships', dealerships),
-  Review: mongoose.model('reviews', reviews)
-};
+module.exports = mongoose.model('reviews', reviews);
